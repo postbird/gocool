41a1e854cbefcb12d7ae28189d32a0337002aa13
postbird <ptbird@yeah.net>
2017-04-06 20:32:41 +0800

    Update sendmail.php

--- sendmail.php
+++ sendmail.php
@@ -46,28 +46,28 @@ function sendMail($to,$title,$content){
     // $mail->Helo = 'Hello smtp.qq.com Server';
 
     //设置发件人的主机域 可有可无 默认为localhost 内容任意，建议使用你的域名
-    $mail->Hostname = 'http://www.cngocool.com';
+    $mail->Hostname = 'http://www.xxxxxxx.com';
 
     //设置发送的邮件的编码 可选GB2312 我喜欢utf-8 据说utf8在某些客户端收信下会乱码
     $mail->CharSet = 'UTF-8';
 
     //设置发件人姓名（昵称） 任意内容，显示在收件人邮件的发件人邮箱地址前的发件人姓名
-    $mail->FromName = '蜂享网站留言';
+    $mail->FromName = 'xxx网站留言';
 
     //smtp登录的账号 这里填入字符串格式的qq号即可
-    $mail->Username ='592312697';
+    $mail->Username ='59xxxxx';
 
     //smtp登录的密码 使用生成的授权码（就刚才叫你保存的最新的授权码）
-    $mail->Password = 'hrutumicyfkmbejh';
+    $mail->Password = 'hrutumxxxxxxh';
 
     //设置发件人邮箱地址 这里填入上述提到的“发件人邮箱”
-    $mail->From = '592312697@qq.com';
+    $mail->From = '5xxxxx@qq.com';
 
     //邮件正文是否为html编码 注意此处是一个方法 不再是属性 true或false
     $mail->isHTML(true);
 
     //设置收件人邮箱地址 该方法有两个参数 第一个参数为收件人邮箱地址 第二参数为给该地址设置的昵称 不同的邮箱系统会自动进行处理变动 这里第二个参数的意义不大
-    $mail->addAddress($to,'蜂享网站留言');
+    $mail->addAddress($to,'xxx享网站留言');
 
     //添加多个收件人 则多次调用方法即可
     // $mail->addAddress('xxx@163.com','lsgo在线通知');
